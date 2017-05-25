51904090ec0d0194cfd21584b8404deb5d55f095
進撃の雯雯老師 <askahu6@gmail.com>
2017-05-25 13:14:07 -0500

    Update sum_all_qtl.R

--- TACC/sum_all_qtl.R
+++ TACC/sum_all_qtl.R
@@ -1,6 +1,6 @@
 
 rm(list=ls())
-
+library(reshape)
 setwd('/home/zhu/rushdata/tacc/rlt')
 
 #get snp2exor result
@@ -20,3 +20,21 @@ rlt <- lapply(f,function(x){load(x);
 })
 rlt.snp2methy <- do.call(rbind,rlt)
 
+#summary
+
+rlt <- list(snp2expr=rlt.snp2expr,methy2expr=rlt.methy2expr,snp2methy=rlt.snp2methy)
+rlt2 <- lapply(rlt,function(x){
+  print(1)
+  x[is.na(x)] <- 1
+  x <- (x*length(x))
+  r <- lapply(1:ncol(x),function(i){
+    y <- x[,i,drop=F]
+    y <- y[y<=0.5,,drop=F]
+    if(length(y)==0){
+      return(NULL)
+    }else{
+      return(melt(y))
+    }
+  })
+  do.call(rbind,r)
+})
